2b222915a687e7d62fbfacb6b492d0f721d50497
HectorPalacio <palaciorivas@gmail.com>
2020-08-17 13:07:13 -0500

    first commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+import 'package:flutter/Material.dart';
+
+class HomePage extends StatelessWidget {
+  final estiloTexto = new TextStyle(fontSize: 25);
+  final conteo = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Título'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Número de Taps', style: estiloTexto),
+          Text('$conteo', style: estiloTexto),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Hola mundo!');
+          //conteo += 1;
+        },
+      ),
+    );
+  }
+}
